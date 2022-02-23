f4f32799e0147306c7d1d4a18cbcfea01804d71c
zhangwei <nstl1999@163.com>
2022-02-24 00:39:02 +0800

    "

--- src/classes/treeViewProvider.ts
+++ src/classes/treeViewProvider.ts
@@ -45,21 +45,19 @@ export class TreeViewProvider implements TreeDataProvider <TreeItemNode> {
         return element;
     }
     getChildren(element?: TreeItemNode | undefined): import('vscode').ProviderResult<TreeItemNode[]> {
-        // return website.map(
-        //     item => new TreeItemNode(
-        //         item.label as string,
-        //         TreeItemCollapsibleState.None as TreeItemCollapsibleState,
-        //     )
-        // );
-        let childs:any = [];
-        website.map(item =>{
-            if(item.type === this.type){
-            childs.push(new TreeItemNode(
-                item.label as string,
-                TreeItemCollapsibleState.None as TreeItemCollapsibleState,
-            ));}
-        });
-        return childs;
+        // if(element){  
+        //     return;
+        // }else{
+            let childs:any = [];
+            website.map(item =>{
+                if(item.type === this.type){
+                childs.push(new TreeItemNode(
+                    item.label as string,
+                    TreeItemCollapsibleState.None as TreeItemCollapsibleState,
+                ));}
+            });
+            return childs;
+        // }
     }
 
     public static initTreeViewItem(){
--- src/website.ts
+++ src/website.ts
@@ -1,10 +1,9 @@
 export default [
     {type:'read',name:'vx_read',label:'微信阅读',site:'https://weread.qq.com/',icon:"vx_read.png"},
-    {type:'interview',name:'vx_rea0000',label:'leetcode',site:'https://leetcode-cn.com/',icon:"vx_read.png"},
-    {type:'interview',name:'222',label:'前端瓶子君算法',site:'https://www.pzijun.cn/',icon:"vx_read.png"},
-    {type:'interview',name:'33333',label:'木易杨前端进阶',site:'https://muyiy.cn/',icon:"vx_read.png"},
-    {type:'other',name:'vx3333222',label:'全历史',site:'https://www.allhistory.com/',icon:"vx_read.png"},
-    {type:'blog',name:'vx_r电放费',label:'廖雪峰博客',site:'https://www.liaoxuefeng.com/',icon:"vx_read.png"},
-    {type:'pictures',name:'vxff0',label:'胶片的味道',https:'http://letsfilm.org/',icon:"vx_read.png"},
-    {type:'pictures',name:'vx_rea0jjj',label:'视觉联盟',site:'http://www.cnu.cc/',icon:"vx_read.png"},
+    {type:'interview',name:'leetcode',label:'leetcode',site:'https://leetcode-cn.com/',icon:"vx_read.png"},
+    {type:'interview',name:'pzijun',label:'前端瓶子君算法',site:'https://www.pzijun.cn/',icon:"pzijun.ico"},
+    {type:'interview',name:'muyiy',label:'木易杨前端进阶',site:'https://muyiy.cn/',icon:"vx_read.png"},
+    {type:'other',name:'all_history',label:'全历史',site:'https://www.allhistory.com/',icon:"vx_read.png"},
+    {type:'blog',name:'liao_blog',label:'廖雪峰博客',site:'https://www.liaoxuefeng.com/',icon:"vx_read.png"},
+    {type:'pictures',name:'CNU',label:'视觉联盟',site:'http://www.cnu.cc/',icon:"vx_read.png"},
 ];
